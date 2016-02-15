830c6ef4d06adf2790cad4018e4675b49ab8079c
jftsang <jmft2@cam.ac.uk>
2016-02-15 14:29:41 +0000

    add some more comments (TODOs) to mercury_load_data.m
    I shall fix the issue of not being able to handle 3D data in a future commit
    (later today).
    
    I shall also change 'data' structures to record positions and velocities as
    two vectors, rather than as six individual fields, e.g.
    data.pos = [1 2 3];
    instead of
    data.x = 1; data.y = 2; data.z = 3;

--- mercury_load_data.m
+++ mercury_load_data.m
@@ -3,6 +3,17 @@
 % - At the moment this can only deal with 2D data. Need to be able to deal
 % with 3D data as well (and ideally to be able to distinguish between them
 % automatically).
+% - There is an assumption at the moment that all frame spacings dt are the
+% same. This might not be the case in general.
+% - It is assumed at the moment that particles are not created or
+% destroyed, and that there is the same number of particles in each frame,
+% with a given index referring to the same particle in different frames. 
+
+% Notes:
+% - The 'dt' found here is the timestep between adjacent entries in the
+% .data file. This is usually much larger than the actual timestep that is
+% used by the simulation. That smaller timestep size will be stored in the
+% .restart file.
 
 function data = mercury_load_data(filename, maxtime)
     tic;
@@ -44,8 +55,10 @@ function data = mercury_load_data(filename, maxtime)
             
             particles(j).x(i) = str2double(line{1});
             particles(j).y(i) = str2double(line{2});
+            particles(j).z(i) = 0; % TODO fix
             particles(j).vx(i) = str2double(line{3});
             particles(j).vy(i) = str2double(line{4});
+            particles(j).vz(i) = 0; % TODO fix
             particles(j).radius(i) = str2double(line{5});
         end
     end
@@ -53,6 +66,7 @@ function data = mercury_load_data(filename, maxtime)
     
     data.nparts = nparts;
     data.ts = ts;
+    data.dt = ts(2) - ts(1); 
     data.particles = particles;
     toc;
 end
