3c74ecc5a5ed796302648e833e486dcd0abda292
jftsang <jmft2@cam.ac.uk>
2016-05-17 00:57:58 +0100

    More tools for drawing cross-sectional data

--- ml_gridveldata.m
+++ ml_gridveldata.m
@@ -1,4 +1,4 @@
-function [yqq,zqq,vxqq,vyqq,vzqq] = ml_gridveldata(fn)
+function [yqq,zqq,rhoqq,vxqq,vyqq,vzqq,Tqq,pqq] = ml_gridveldata(fn)
     out = importdata(fn);
     yqs = out.data(:,3);
     zqs = out.data(:,4);
@@ -6,13 +6,20 @@ function [yqq,zqq,vxqq,vyqq,vzqq] = ml_gridveldata(fn)
     vxqs = out.data(:,6);
     vyqs = out.data(:,7);
     vzqs = out.data(:,8);
+    Tqs = out.data(:,12);
+    pqs = out.data(:,13);
     
     [yqq, zqq] = meshgrid( ...
         linspace(min(yqs), max(yqs), 128), ...
         linspace(min(zqs), max(zqs), 128) );
     [yqq, zqq, rhoqq] = griddata(yqs, zqs, rhoqs, yqq, zqq);
-    rhoqq(rhoqq==0) = nan; % so that regions of zero density aren't painted
+    rhoqq(rhoqq < 0.1) = nan; % so that regions of zero density aren't painted
     [yqq, zqq, vxqq] = griddata(yqs, zqs, vxqs, yqq, zqq);
     [yqq, zqq, vyqq] = griddata(yqs, zqs, vyqs, yqq, zqq);
     [yqq, zqq, vzqq] = griddata(yqs, zqs, vzqs, yqq, zqq);
+    [yqq, zqq, Tqq] = griddata(yqs, zqs, Tqs, yqq, zqq);
+    Tqq(isnan(rhoqq)) = nan;
+
+    [yqq, zqq, pqq] = griddata(yqs, zqs, pqs, yqq, zqq);
+    pqq(isnan(rhoqq)) = nan;
 end
